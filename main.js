f19752950dc6bd612c0d3bd5e3ade5a708f7cb39
Chloe Seiler <chloe.seiler123@live.fr>
2019-03-13 23:05:19 +0100

    people cards

---
+++ main.js
@@ -0,0 +1,193 @@
+$(document).ready(function() {
+
+    $(".parent1").click(function() {
+        $( ".parent1" ).addClass("turnTopCard");
+        $( ".card_content1" ).addClass("turnBottomCard");
+      });
+
+      $(".parent2").click(function() {
+        $( ".parent2" ).addClass("turnTopCard");
+        $( ".card_content2" ).addClass("turnBottomCard");
+      });
+
+      $(".parent3").click(function() {
+        $( ".parent3" ).addClass("turnTopCard");
+        $( ".card_content3" ).addClass("turnBottomCard");
+      });
+
+      $(".parent4").click(function() {
+        $( ".parent4" ).addClass("turnTopCard");
+        $( ".card_content4" ).addClass("turnBottomCard");
+      });
+
+      $(".parent5").click(function() {
+        $( ".parent5" ).addClass("turnTopCard");
+        $( ".card_content5" ).addClass("turnBottomCard");
+      });
+
+      $(".parent6").click(function() {
+        $( ".parent6" ).addClass("turnTopCard");
+        $( ".card_content6" ).addClass("turnBottomCard");
+      });
+
+      $(".parent7").click(function() {
+        $( ".parent7" ).addClass("turnTopCard");
+        $( ".card_content7" ).addClass("turnBottomCard");
+      });
+
+      $(".parent8").click(function() {
+        $( ".parent8" ).addClass("turnTopCard");
+        $( ".card_content8" ).addClass("turnBottomCard");
+      });
+
+      $(".parent9").click(function() {
+        $( ".parent9" ).addClass("turnTopCard");
+        $( ".card_content9" ).addClass("turnBottomCard");
+      });
+
+      $(".parent10").click(function() {
+        $( ".parent10" ).addClass("turnTopCard");
+        $( ".card_content10" ).addClass("turnBottomCard");
+      });
+
+      $(".parent11").click(function() {
+        $( ".parent11" ).addClass("turnTopCard");
+        $( ".card_content11" ).addClass("turnBottomCard");
+      });
+
+      $(".parent12").click(function() {
+        $( ".parent12" ).addClass("turnTopCard");
+        $( ".card_content12" ).addClass("turnBottomCard");
+      });
+
+      $(".parent13").click(function() {
+        $( ".parent13" ).addClass("turnTopCard");
+        $( ".card_content13" ).addClass("turnBottomCard");
+      });
+
+      $(".parent14").click(function() {
+        $( ".parent14" ).addClass("turnTopCard");
+        $( ".card_content14" ).addClass("turnBottomCard");
+      });
+
+      $(".parent15").click(function() {
+        $( ".parent15" ).addClass("turnTopCard");
+        $( ".card_content15" ).addClass("turnBottomCard");
+      });
+
+      $(".parent16").click(function() {
+        $( ".parent16" ).addClass("turnTopCard");
+        $( ".card_content16" ).addClass("turnBottomCard");
+      });
+
+      $(".parent17").click(function() {
+        $( ".parent17" ).addClass("turnTopCard");
+        $( ".card_content17" ).addClass("turnBottomCard");
+      });
+
+      $(".parent18").click(function() {
+        $( ".parent18" ).addClass("turnTopCard");
+        $( ".card_content18" ).addClass("turnBottomCard");
+      });
+
+      $(".parent19").click(function() {
+        $( ".parent19" ).addClass("turnTopCard");
+        $( ".card_content19" ).addClass("turnBottomCard");
+      });
+
+      $(".card_content1").click(function() {
+        $( ".parent1" ).removeClass("turnTopCard");
+        $( ".card_content1" ).removeClass("turnBottomCard");
+      });
+
+      $(".card_content2").click(function() {
+        $( ".parent2" ).removeClass("turnTopCard");
+        $( ".card_content2" ).removeClass("turnBottomCard");
+      });
+
+      $(".card_content3").click(function() {
+        $( ".parent3" ).removeClass("turnTopCard");
+        $( ".card_content3" ).removeClass("turnBottomCard");
+      });
+
+      $(".card_content4").click(function() {
+        $( ".parent4" ).removeClass("turnTopCard");
+        $( ".card_content4" ).removeClass("turnBottomCard");
+      });
+
+      $(".card_content5").click(function() {
+        $( ".parent5" ).removeClass("turnTopCard");
+        $( ".card_content5" ).removeClass("turnBottomCard");
+      });
+
+      $(".card_content6").click(function() {
+        $( ".parent6" ).removeClass("turnTopCard");
+        $( ".card_content6" ).removeClass("turnBottomCard");
+      });
+
+      $(".card_content7").click(function() {
+        $( ".parent7" ).removeClass("turnTopCard");
+        $( ".card_content7" ).removeClass("turnBottomCard");
+      });
+
+      $(".card_content8").click(function() {
+        $( ".parent8" ).removeClass("turnTopCard");
+        $( ".card_content8" ).removeClass("turnBottomCard");
+      });
+
+      $(".card_content9").click(function() {
+        $( ".parent9" ).removeClass("turnTopCard");
+        $( ".card_content9" ).removeClass("turnBottomCard");
+      });
+
+      $(".card_content10").click(function() {
+        $( ".parent10" ).removeClass("turnTopCard");
+        $( ".card_content10" ).removeClass("turnBottomCard");
+      });
+
+      $(".card_content11").click(function() {
+        $( ".parent11" ).removeClass("turnTopCard");
+        $( ".card_content11" ).removeClass("turnBottomCard");
+      });
+
+      $(".card_content12").click(function() {
+        $( ".parent12" ).removeClass("turnTopCard");
+        $( ".card_content12" ).removeClass("turnBottomCard");
+      });
+
+      $(".card_content13").click(function() {
+        $( ".parent13" ).removeClass("turnTopCard");
+        $( ".card_content13" ).removeClass("turnBottomCard");
+      });
+
+      $(".card_content14").click(function() {
+        $( ".parent14" ).removeClass("turnTopCard");
+        $( ".card_content14" ).removeClass("turnBottomCard");
+      });
+
+      $(".card_content15").click(function() {
+        $( ".parent15" ).removeClass("turnTopCard");
+        $( ".card_content15" ).removeClass("turnBottomCard");
+      });
+
+      $(".card_content16").click(function() {
+        $( ".parent16" ).removeClass("turnTopCard");
+        $( ".card_content16" ).removeClass("turnBottomCard");
+      });
+
+      $(".card_content17").click(function() {
+        $( ".parent17" ).removeClass("turnTopCard");
+        $( ".card_content17" ).removeClass("turnBottomCard");
+      });
+
+      $(".card_content18").click(function() {
+        $( ".parent18" ).removeClass("turnTopCard");
+        $( ".card_content18" ).removeClass("turnBottomCard");
+      });
+
+      $(".card_content19").click(function() {
+        $( ".parent19" ).removeClass("turnTopCard");
+        $( ".card_content19" ).removeClass("turnBottomCard");
+      });
+
+});
